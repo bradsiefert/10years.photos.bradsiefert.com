c99b1cf1130ddd402aab5e1b08651ccd16e7a250
Brad Siefert <bradsiefert@icloud.com>
2023-09-05 13:34:09 -0500

    Small sharing change

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <header>
     <navbar />
   </header>
+
   <main>
     <NuxtPage/>
   </main>
@@ -19,7 +20,7 @@
     description: 'A photo book of the first 10 Years of photographs by Brad Siefert.',
     ogTitle: '10 Years of Photos by Brad Siefert',
     ogDescription: 'A photo book of the first 10 Years of photographs by Brad Siefert.',
-    ogImage: 'https://10years.photos.bradsiefert.com/sharing-tile.jpg',
+    ogImage: 'https://10years.photos.bradsiefert.com/sharing.jpg',
     twitterCard: 'summary_large_image'
   })
 </script>
