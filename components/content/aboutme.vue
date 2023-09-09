7c6e1e1125b17588c00b8d1c74285e6bfd26ab58
Brad Siefert <bradsiefert@icloud.com>
2023-09-09 14:45:13 -0500

    Adding About Me

---
+++ components/content/aboutme.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-xl-11 m-auto">
+        <div class="text-block">
+          <div class="row">
+            <div class="col-sm-7 col-md-4 col-lg-3 ms-auto me-auto">
+              <img
+                src="/aboutme.jpg"
+                class="img-fluid mb-5"
+                alt="Brad Siefert self portrait"
+              >
+            </div>
+            <div class="col-md-12 col-lg-9">
+              <h2>About Me</h2>
+              <p>
+                I spend my days designing web and mobile apps, but an incredible part of my time and energy is focused on being a better photographer. Learning the fundamentals of photography took me a very long time before it clicked for me. From that time, I have been driven to learn more and more about how to make great images and how to tell small stories with those images. I am married to a wonderful woman, Nicole, who never complains or shames me when I want to take photos. For that, I am incredibly grateful.
+              </p>
+              <h3 class="text-center text-sm-center text-md-start">Self Portrait</h3>
+              <div class="details">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md date-time">2015/05/28 8:00pm</div>
+                    <div class="col-md photo-metadata">35mm f/4 1/180th 200 ISO</div>
+                    <div class="col-md gps-location">41°55’49” N 87°42’23” W</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
