e264271c0c11e61372f83fe2b43c12e6515c8809
Brad Siefert <bradsiefert@icloud.com>
2023-09-09 22:05:42 -0500

    Working on bottom navigation

--- components/footie.vue
+++ components/footie.vue
@@ -1,26 +1,19 @@
 <template>
   <footer class="footer">
+    <nav class="navbar navbar-expand">
       <div class="container">
-        <div class="row">
-          <div class="col">
-            <NuxtLink  class="nav-link" v-if="prev" :to="prev._path">Prev</NuxtLink>
-          </div>
+        <NuxtLink  class="nav-link" v-if="prev" :to="prev._path">Prev</NuxtLink>
 
-          <div class="col">
-            <NuxtLink class="nav-link" to="/tableofcontents">
-              Table of Contents
-            </NuxtLink>
-          </div>
+        <NuxtLink class="nav-link" to="/tableofcontents">
+          Table of Contents
+        </NuxtLink>
 
-          <div class="col">
-            <NuxtLink class="nav-link" v-if="next" :to="next._path">Next</NuxtLink>
-          </div>
-        </div>
-
+        <NuxtLink class="nav-link" v-if="next" :to="next._path">Next</NuxtLink>
       </div>
+    </nav>
   </footer>
 </template>
 
 <script setup lang="ts">
-const { prev, next, navigation } = useContent()
+const { prev, next } = useContent()
 </script>
