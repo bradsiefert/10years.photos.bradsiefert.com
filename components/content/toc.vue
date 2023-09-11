e6d663a9c88650c5f3e3d16a70abafe23955326c
Brad Siefert <bradsiefert@icloud.com>
2023-09-10 21:53:51 -0500

    Cleaning up toc

--- components/content/toc.vue
+++ components/content/toc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-4 mb-3">
+  <div class="container toc-wrap">
     <h1>Table of Contents</h1>
     <div class="row">
       <div class="col-lg">
@@ -14,23 +14,22 @@
         </div>
       </div>
 
-      <div class="col-lg">
+      <!--<div class="col-lg">
         <div class="text-block pt-0 pb-0">
           <ContentList :query="column2" path="/" v-slot="{ list }">
-            <ol start="1" class="toc mb-0">
+            <ol start="6" class="toc mb-0">
               <li v-for="article in list" :key="article._path">
                 <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
               </li>
             </ol>
           </ContentList>
         </div>
-      </div>
-
+      </div>-->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const column1 = { limit: 0 }
-const column2 = { skip: 0 }
+  const column1 = { limit: 0 }
+  const column2 = { skip: 5 }
 </script>
--- layouts/image-post.vue
+++ layouts/image-post.vue
@@ -9,17 +9,15 @@
         <p>Loading...</p>
       </template>
     </ContentRenderer>
-    <div class="width-contain">
-      <div class="photo">
-        <img :src="'' + doc.image.src" :alt="doc.title">
-      </div>
-      <div class="details">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md">{{ doc.details.date }}</div>
-            <div class="col-md">{{ doc.details.settings }}</div>
-            <div class="col-md">{{ doc.details.location }}</div>
-          </div>
+    <div class="photo">
+      <img :src="'' + doc.image.src" :alt="doc.title">
+    </div>
+    <div class="details">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md">{{ doc.details.date }}</div>
+          <div class="col-md">{{ doc.details.settings }}</div>
+          <div class="col-md">{{ doc.details.location }}</div>
         </div>
       </div>
     </div>
