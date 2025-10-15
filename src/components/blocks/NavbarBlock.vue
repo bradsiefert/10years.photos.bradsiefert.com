7e9c90715168a779eb752f5e360a2a13055abc4f
Brad Siefert <bradsiefert@users.noreply.github.com>
2025-10-14 23:30:15 -0500

    Initial commit (main)

---
+++ src/components/blocks/NavbarBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav class="navbar justify-content-center">
+      <div class="container pl-0 pr-0">
+        <div class="navbar-brand d-none d-sm-block">
+          Brad Siefert
+        </div>
+        <div class="abs-center-x">
+          <router-link to="/">
+            <img src="/static/images/logo.svg" alt="10 Years of Photos, 2007 - 2017">
+          </router-link>
+        </div>
+        <div class="d-none d-sm-block ml-auto">
+          <a class="nav-link" target="_blank" href="http://photos.bradsiefert.com">
+            <img width="32" src="/static/images/bks-camera.png"
+                 alt="Brad Siefert camera illustration logo">
+          </a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ src/components/templates/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 col-lg-10 m-auto">
+          <img src="static/images/intro.jpg" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+    <footer-block :next="next" :previous="previous"/>
+  </main>
+</template>
+
+<script>
+import FooterBlock from '@/components/blocks/FooterBlock';
+
+export default {
+  components: {
+    FooterBlock,
+  },
+  props: {
+    previous: String,
+    next: String,
+  },
+};
+</script>
