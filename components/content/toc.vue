1259eaace260600bd8717878d8db0f61ed8c3f31
Brad Siefert <bradsiefert@icloud.com>
2026-02-06 17:19:27 -0600

    Styling and cleaning up TOC page

--- components/content/toc.vue
+++ components/content/toc.vue
@@ -1,35 +1,96 @@
 <template>
   <div class="container toc-wrap">
-    <h1>Table of Contents</h1>
     <div class="row">
-      <div class="col-lg">
+      <div class="col-lg-6 toc-column">
+        <h1 class="mb-4">Table of Contents</h1>
         <div class="text-block pt-0 pb-0">
-          <ContentList :query="column1" path="/" v-slot="{ list }">
-            <ol start="1" class="toc mb-0">
-              <li v-for="article in list" :key="article._path">
-                <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
-              </li>
-            </ol>
-          </ContentList>
+          <ul class="toc mb-0">
+            <li><strong><NuxtLink to="/about/02">02 About Me</NuxtLink></strong></li>
+            <li><strong><NuxtLink to="/about/03">03 Ten Years In</NuxtLink></strong></li>
+            <li><strong><NuxtLink to="/about/04">04 Day 1 of 3653</NuxtLink></strong></li>
+            <li class="toc-section"><strong><NuxtLink to="/places/06">06 Places</NuxtLink></strong></li>
+            <li><NuxtLink to="/places/07">07 Beautiful Indiana barn I drove past at sunset</NuxtLink></li>
+            <li><NuxtLink to="/places/08">08 Reflection of Mount Moran at Oxbow Bend</NuxtLink></li>
+            <li><NuxtLink to="/places/09">09 David gazing at the mountains in Grand Teton National Park</NuxtLink></li>
+            <li><NuxtLink to="/places/10">10 Morning of our wedding at the dock</NuxtLink></li>
+            <li><NuxtLink to="/places/11">11 Crashing waves at Sunset Cliffs in San Diego</NuxtLink></li>
+            <li><NuxtLink to="/places/12">12 The view from the ferry to Ometepe Island on my honeymoon</NuxtLink></li>
+            <li><NuxtLink to="/places/13">13 Sunset view at the Lower Falls of Yellowstone River</NuxtLink></li>
+            <li><NuxtLink to="/places/14">14 Happened upon a moose on the trail in Grand Teton Natl Park</NuxtLink></li>
+            <li><NuxtLink to="/places/15">15 White horse on the shore of a kayak tour on my honeymoon</NuxtLink></li>
+            <li><NuxtLink to="/places/16">16 Vanessa taking in the view of Rabbit Island</NuxtLink></li>
+            <li><NuxtLink to="/places/17">17 Stream running through Grand Teton National Park</NuxtLink></li>
+            <li><NuxtLink to="/places/18">18 Young boy jumps off a dock at sunset on Ometepe Island</NuxtLink></li>
+            <li><NuxtLink to="/places/19">19 The best sunset I've ever seen, Ometepe Island, Nicaragua</NuxtLink></li>
+            <li><NuxtLink to="/places/20">20 Lake Michigan sunset from Saugatuck Dunes State Park</NuxtLink></li>
+            <li><NuxtLink to="/places/21">21 Self portrait from near the saddle of the Middle Teton</NuxtLink></li>
+            <li><NuxtLink to="/places/22-23">22 Sunset panorama of Bear Lake in Rocky Mountain Natl Park</NuxtLink></li>
+            <li><NuxtLink to="/places/24">24 Rural church in Auburn, WI</NuxtLink></li>
+            <li><NuxtLink to="/places/25">25 Man prays in the early morning at a church in Granada, NI</NuxtLink></li>
+            <li><NuxtLink to="/places/26">26 Michael gazing over Jenny Lake in Grand Teton National Park</NuxtLink></li>
+            <li><NuxtLink to="/places/27">27 Scissor graffiti on the Matilija Dam near Ojai, CA</NuxtLink></li>
+            <li><NuxtLink to="/places/28">28 Long exposure at Cowboy Cafe in Dubios, WY</NuxtLink></li>
+            <li><NuxtLink to="/places/29">29 Long exposure of the historic Logan Theatre sign</NuxtLink></li>
+            <li><NuxtLink to="/places/30">30 Fiery sunset at the Logan Theatre</NuxtLink></li>
+            <li><NuxtLink to="/places/31">31 Light streaks long exposure at LaSalle Bridge</NuxtLink></li>
+            <li><NuxtLink to="/places/32">32 Nicole at Balance Rock in Arches National Park</NuxtLink></li>
+            <li><NuxtLink to="/places/33">33 The historic Delicate Arch in Arches National Park</NuxtLink></li>
+            <li><NuxtLink to="/places/34">34 The Milky Way at Balance Rock</NuxtLink></li>
+            <li><NuxtLink to="/places/35">35 The Milky Way at Delicate Arch</NuxtLink></li>
+            <li><NuxtLink to="/places/36">36 Light painting the Rabbit Island sign</NuxtLink></li>
+            <li><NuxtLink to="/places/37">37 The Northern Lights dancing around at Rabbit Island</NuxtLink></li>
+            <li class="toc-section"><strong><NuxtLink to="/4/people/38">38 People</NuxtLink></strong></li>
+            <li><NuxtLink to="/4/people/39">39 Waiting on the Green Line</NuxtLink></li>
+            <li><NuxtLink to="/4/people/40">40 Riding the swings with some of my best friends at Navy Pier</NuxtLink></li>
+          </ul>
         </div>
       </div>
-
-      <!--<div class="col-lg">
+      <div class="col-lg-6 toc-column">
         <div class="text-block pt-0 pb-0">
-          <ContentList :query="column2" path="/" v-slot="{ list }">
-            <ol start="6" class="toc mb-0">
-              <li v-for="article in list" :key="article._path">
-                <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
-              </li>
-            </ol>
-          </ContentList>
+          <ul class="toc mb-0">
+            <li><NuxtLink to="/4/people/41">41 Surfer does a backflip cliff jump</NuxtLink></li>
+            <li><NuxtLink to="/people/42">42 Double exposure portrait of Jon</NuxtLink></li>
+            <li><NuxtLink to="/people/43">43 Goodbye portrait of Buth leaving Rabbit Island</NuxtLink></li>
+            <li><NuxtLink to="/people/44">44 Stylized portrait of Esther at 63rd Street Beach</NuxtLink></li>
+            <li><NuxtLink to="/people/45">45 The 83rd portrait, Covert St. Brooklyn, NY</NuxtLink></li>
+            <li><NuxtLink to="/people/46">46 Portrait of the Rev. Dr. Tony Campolo</NuxtLink></li>
+            <li><NuxtLink to="/people/47">47 Portrait after Bethel Lutheran Church's 125th Anniversary</NuxtLink></li>
+            <li><NuxtLink to="/people/48">48 Sarah at Chicago Botanic Gardens</NuxtLink></li>
+            <li><NuxtLink to="/people/49">49 Fall portrait of Nicole at Ohio Blvd in Terre Haute, IN</NuxtLink></li>
+            <li><NuxtLink to="/people/50">50 Portrait of my brother Matt in downtown Denver, CO</NuxtLink></li>
+            <li><NuxtLink to="/people/51">51 Company work retreat at Warren Dunes State Park</NuxtLink></li>
+            <li><NuxtLink to="/people/52">52 Swirling bokeh portrait of John in Logan Square</NuxtLink></li>
+            <li><NuxtLink to="/people/53">53 Anthony loves macaroons, Montreal, QC</NuxtLink></li>
+            <li><NuxtLink to="/people/54">54 Senior portrait of Kati in Provin Park, Grand Rapids, MI</NuxtLink></li>
+            <li><NuxtLink to="/people/55">55 Senior portrait of Camron in Grand Rapids, MI</NuxtLink></li>
+            <li><NuxtLink to="/people/56">56 Anthony &amp; Laura engagement session at Art Institute of Chicago</NuxtLink></li>
+            <li><NuxtLink to="/people/57">57 Sneakily shooting Jason's engagement proposal to Blue</NuxtLink></li>
+            <li><NuxtLink to="/people/58">58 Portrait of Will at Chicago Music Exchange</NuxtLink></li>
+            <li><NuxtLink to="/people/59">59 Portrait of Jimmy in studio</NuxtLink></li>
+            <li><NuxtLink to="/people/60">60 Portrait of Charity while apple picking</NuxtLink></li>
+            <li><NuxtLink to="/people/61">61 Studio portrait of my first pet, my bearded dragon Richard Parker</NuxtLink></li>
+            <li class="toc-section"><strong><NuxtLink to="/5/instantfilm/62">62 Instant Film</NuxtLink></strong></li>
+            <li><NuxtLink to="/5/instantfilm/63">63 Portrait of Will at the Logan Square mural</NuxtLink></li>
+            <li><NuxtLink to="/5/instantfilm/64">64 Double exposure Polaroid portrait of The 83rd</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/65">65 Double exposure Polaroid portrait of Abbi Rajasekhar</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/66">66 Double exposure Polaroid of Anthony at the Wolfpack retreat</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/67">67 Double exposure Polaroid of Josh at the Wolfpack retreat</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/68">68 Double exposure Polaroid of Jason at the Wolfpack retreat</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/69">69 Polaroid negative portrait of Shiraz</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/70">70 Expired film Polaroid portrait of Nathanael at Lake Michigan</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/71">71 Polaroid of Andrew surfing at Rabbit Island</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/72">72 Double exposure Polaroid portrait of Rev. Dr. Tony Campolo</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/73">73 Double exposure Polaroid portrait of Jimmy Spencer Jr.</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/74">74 Polaroid portrait of my neighbor's mother Goyita in studio</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/75">75 Negative Polaroid portrait of a little girl at New Moms</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/76">76 Instant film snapshot of Laura's birthday party</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/77">77 How do I even describe this photo? No need.</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/78">78 Sneakily shot Polaroid of Jeff in French Lick, IN</NuxtLink></li>
+            <li><NuxtLink to="/instantfilm/79">79 Polaroid of Jeff &amp; Nicole processing at our wedding</NuxtLink></li>
+            <li><strong><NuxtLink to="/endnotes/80">80 Thank Yous, Colophon, &amp; Contact</NuxtLink></strong></li>
+          </ul>
         </div>
-      </div>-->
+      </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-  const column1 = { limit: 0 }
-  const column2 = { skip: 5 }
-</script>
