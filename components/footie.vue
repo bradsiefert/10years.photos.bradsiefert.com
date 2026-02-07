c8187f8607bb80578ecc4dda04286ba72262d412
Brad Siefert <bradsiefert@icloud.com>
2026-02-07 14:33:25 -0600

    Footer clean up and adding footer icons

--- components/footie.vue
+++ components/footie.vue
@@ -3,17 +3,25 @@
     <nav class="navbar navbar-expand">
       <div class="container">
         <div class="float-start">
-          <NuxtLink  class="nav-link" v-if="prev" :to="prev._path">Prev</NuxtLink>
+          <NuxtLink class="nav-link d-flex align-items-center gap-2" v-if="prev" :to="prev._path">
+            <PhArrowLeft :size="24" />
+            Prev
+          </NuxtLink>
         </div>
 
         <div class="abs-center-x" style="top: 0;">
-          <NuxtLink class="nav-link" to="/tableofcontents">
-            Table of Contents
+          <NuxtLink class="nav-link d-flex align-items-center gap-2" to="/tableofcontents">
+            <PhImages :size="24" />
+            <span class="d-none d-sm-inline">Table of Contents</span>
+            <span class="d-inline d-sm-none">TOC</span>
           </NuxtLink>
         </div>
 
         <div class="float-end">
-          <NuxtLink class="nav-link" v-if="next" :to="next._path">Next</NuxtLink>
+          <NuxtLink class="nav-link d-flex align-items-center gap-2" v-if="next" :to="next._path">
+            Next
+            <PhArrowRight :size="24" />
+          </NuxtLink>
         </div>
       </div>
     </nav>
@@ -21,5 +29,6 @@
 </template>
 
 <script setup lang="ts">
+import { PhArrowLeft, PhArrowRight, PhImages } from '@phosphor-icons/vue'
 const { prev, next } = useContent()
 </script>
